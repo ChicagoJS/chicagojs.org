27ecfe55418776fa6bc974c8c9bad4970182c110
SebastianKurp <sebbykurp@gmail.com>
2018-10-21 19:41:00 -0500

    Added boiler plate for the small blurb and the levels of sponsorship.

--- src/pages/sponsors.js
+++ src/pages/sponsors.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Helmet } from "react-helmet"
+import styles from "../styles/sponsors.module.css"
 import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
 import Layout from '../components/Layout'
@@ -11,17 +12,49 @@ export default class sponsors extends React.Component {
         <Helmet>
             <title>Sponsors</title>
             <meta name="description" content="Sponsors for ChicagoJS" />
-	          <meta name="keywords" content="Sponosors, ChicagoJS, Javascript,Js" />
+	          <meta name="keywords" content="Sponsors, ChicagoJS, Javascript,Js" />
             <meta name="author" content="Sebastian Kurpiel" />
             {/* Bootstrap style sheet */}
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"></link>    
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"></link>
+            <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet"></link>    
         </Helmet>
-        <div class="jumbotron jumbotron-fluid">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <h1 class="heading">Sponsors</h1>
+        <div className={`jumbotron jumbotron-fluid ${styles.jumbotron}`}>
+            <div className="container">
+                <div className="row justify-content-center">
+                    <h1 className={styles.heading}>Sponsors</h1>
                 </div>
             </div>
+        </div>
+        <div className="container">
+            <div className="row text-center">
+                <div className="heading">
+                    <p className={styles.blurb}>
+                      A Small Intro, maybe for thanking the sponsors... This text should be centered... Let's see if it worked!
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div>
+          <div className="container">
+            <div className={styles.headingContainer}>
+                <h2 className={styles.sponsorsLevel}>Platinum</h2>
+            </div>
+          </div>
+          <div className="container">
+            <div className={styles.headingContainer}>
+                <h2 className={styles.sponsorsLevel}>Gold</h2>
+            </div>
+          </div> 
+          <div className="container">
+            <div className={styles.headingContainer}>
+                <h2 className={styles.sponsorsLevel}>Sliver</h2>
+            </div>
+          </div> 
+          <div className="container">
+            <div className={styles.headingContainer}>
+                <h2 className={styles.sponsorsLevel}>Bronze</h2>
+            </div>
+          </div>  
         </div>
       </div>
     )

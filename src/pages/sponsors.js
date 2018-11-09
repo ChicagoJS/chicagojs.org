6c24834935626d0f5a47b396b11f47798b11ece4
SebastianKurp <sebbykurp@gmail.com>
2018-11-08 19:36:17 -0600

    Swapped page to stateful component

--- src/pages/sponsors.js
+++ src/pages/sponsors.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import { Helmet } from "react-helmet"
 import styles from "../styles/sponsors.module.css"
-import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
-import Layout from '../components/Layout'
+import PropTypes from 'prop-types' // do we even need this because we don't have any props on this page?
+import { graphql } from 'gatsby'
 
 
 const testData = [
@@ -68,32 +67,21 @@ const testData = [
 		]
 	  },
 ]
-export default class sponsors extends React.Component {
-  constructor(props){
-    super(props);
-
-    this.state ={
-      sponsorsData: null
-    }
-  }
-
-  componentDidMount() {
-		this.setState({sponsorsData: testData});
-	}
+const SponsorsPage = ( {data} ) => {
 	
-	getImageUrl = (companyName) => {
+	function getImageUrl(companyName) {
 		const URLRegex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/ ;
 		return URLRegex.test(companyName) ? companyName : 'https://res.cloudinary.com/chicagojs/image/upload/2018Sponsors/' + companyName;
 	}
 
-	grabTieredSponsors = (tier) => {
+	 function grabTieredSponsors(tier) {
 		const sponsorsArray = testData.find(data => data.title === tier);
 		if(sponsorsArray.items.length > 0) {
 			return sponsorsArray.items.map((i,index) => (
 				<div className={`col-md-6 col-lg-4 ${styles.sponsorCard}`}>
 					<div className="card border-0 transform-on-hover">
 						<a className="lightbox" href="https://www.amazon.com/" target="_blank"> {/*swap amazon url for company url at one point*/}
-							<img src={this.getImageUrl(i.image)} alt="Card Image" className="card-img-top" />
+							<img src={getImageUrl(i.image)} alt="Card Image" className="card-img-top" />
 						</a>
 						<div className={`${styles.cardBody} ${styles[tier]}`}>
 							<h6 className={styles.sponsorName}>{`${i.name}`}</h6>
@@ -104,69 +92,69 @@ export default class sponsors extends React.Component {
 		}
 	}
 
-  render() {
-    return (
-      <div className="page-container">
-        <Helmet>
-            <title>Sponsors</title>
-            <meta name="description" content="Sponsors for ChicagoJS" />
-	          <meta name="keywords" content="Sponsors, ChicagoJS, Javascript,Js" />
-            <meta name="author" content="Sebastian Kurpiel" />
-            {/* Bootstrap style sheet */}
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"></link>
-            <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet"></link>    
-        </Helmet>
-        <div className={`jumbotron jumbotron-fluid ${styles.jumbotron}`}>
-            <div className="container">
-                <div className="row justify-content-center">
-                    <h1 className={styles.heading}>Sponsors</h1>
-                </div>
-            </div>
-        </div>
-        <div className="container">
-            <div className="row text-center">
-                <div className="heading">
-                    <p className={styles.blurb}>
-                      A Small Intro, maybe for thanking the sponsors... This text should be centered... Let's see if it worked!
-                    </p>
-                </div>
-            </div>
-        </div>
-        <div className={`gallery-block `}>
+	return (
+		<div className="page-container">
+			<Helmet>
+					<title>Sponsors</title>
+					<meta name="description" content="Sponsors for ChicagoJS" />
+					<meta name="keywords" content="Sponsors, ChicagoJS, Javascript,Js" />
+					<meta name="author" content="Sebastian Kurpiel" />
+					{/* Bootstrap style sheet */}
+					<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"></link>
+					<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet"></link>    
+			</Helmet>
+			<div className={`jumbotron jumbotron-fluid ${styles.jumbotron}`}>
 					<div className="container">
-            <div className={styles.headingContainer}>
-                <h2 className={styles.sponsorsLevel}>Platinum</h2>
-            </div>
-            <div className={"row"} >
-                {this.grabTieredSponsors('platinum')}
-            </div>
-          </div>
-          <div className="container">
-            <div className={styles.headingContainer}>
-                <h2 className={styles.sponsorsLevel}>Gold</h2>
-            </div>
-            <div className={"row"} >
-                {this.grabTieredSponsors('gold')}
-            </div>
-          </div> 
-          <div className="container">
-            <div className={styles.headingContainer}>
-                <h2 className={styles.sponsorsLevel}>Sliver</h2>
-            </div>
-            <div className="row" >
-								{this.grabTieredSponsors('sliver')}
-            </div>
-          </div> 
-          <div className="container">
-            <div className={styles.headingContainer}>
-                <h2 className={styles.sponsorsLevel}>Bronze</h2>
-            </div>
-            <div className="row">
-								{this.grabTieredSponsors('bronze')}
-            </div>
-          </div>  
-        </div>
-      </div>
-    )
-  }
+							<div className="row justify-content-center">
+									<h1 className={styles.heading}>Sponsors</h1>
+							</div>
+					</div>
+			</div>
+			<div className="container">
+					<div className="row text-center">
+							<div className="heading">
+									<p className={styles.blurb}>
+										A Small Intro, maybe for thanking the sponsors... This text should be centered... Let's see if it worked!
+									</p>
+							</div>
+					</div>
+			</div>
+			<div className={`gallery-block `}>
+				<div className="container">
+					<div className={styles.headingContainer}>
+							<h2 className={styles.sponsorsLevel}>Platinum</h2>
+					</div>
+					<div className={"row"} >
+							{grabTieredSponsors('platinum')}
+					</div>
+				</div>
+				<div className="container">
+					<div className={styles.headingContainer}>
+							<h2 className={styles.sponsorsLevel}>Gold</h2>
+					</div>
+					<div className={"row"} >
+							{grabTieredSponsors('gold')}
+					</div>
+				</div> 
+				<div className="container">
+					<div className={styles.headingContainer}>
+							<h2 className={styles.sponsorsLevel}>Sliver</h2>
+					</div>
+					<div className="row" >
+							{grabTieredSponsors('sliver')}
+					</div>
+				</div> 
+				<div className="container">
+					<div className={styles.headingContainer}>
+							<h2 className={styles.sponsorsLevel}>Bronze</h2>
+					</div>
+					<div className="row">
+							{grabTieredSponsors('bronze')}
+					</div>
+				</div>  
+			</div>
+		</div>
+	)
 }
+
+export default SponsorsPage;

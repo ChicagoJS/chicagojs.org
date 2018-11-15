d19cdc6746ecab6e047e18b8aed4214ad685f4b9
Peter Piekarczyk <peterpme@users.noreply.github.com>
2018-11-15 16:05:34 -0600

    @peterpme/update header links / stub out main pages (#23)
    @peterpme/update header links & stub out main pages

---
+++ src/pages/meetups/index.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import get from 'lodash.get'
+import Layout from '../../components/Layout'
+
+const MeetupLink = ({ name, description, meetupUrl }) => (
+  <div className="col-sm-6">
+    <div className="card mb-2" style={{ minHeight: 188 }}>
+      <div className="card-body">
+        <h5 className="card-title">{name}</h5>
+        <p className="card-text">{description}</p>
+        <a href={meetupUrl} className="card-link">
+          Learn More
+        </a>
+      </div>
+    </div>
+  </div>
+)
+
+export default class MeetupsPage extends React.Component {
+  render() {
+    const meetups = get(this.props, 'data.allLocalMeetupsJson.edges', [])
+    return (
+      <Layout title="Local Meetups">
+        <h1 className="text-center mb-4">Local Meetups</h1>
+        <div className="container">
+          <div className="row">
+            {meetups.map(m => (
+              <MeetupLink {...m.node} />
+            ))}
+          </div>
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export const query = graphql`
+  query LocalMeetupsQuery {
+    allLocalMeetupsJson {
+      edges {
+        node {
+          name
+          description
+          meetupUrl
+        }
+      }
+    }
+  }
+`

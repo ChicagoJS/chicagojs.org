b4c362308e6b2f7113a2a3de940b15f634df69b7
Sebastian <sebbykurp@gmail.com>
2018-11-16 13:06:56 -0600

    Create SponsorRow

--- src/pages/sponsors/index.js
+++ src/pages/sponsors/index.js
@@ -8,22 +8,32 @@ const getImageUrl = companyName => {
   return isValidUrl(companyName) ? companyName : getCloudinaryImage(`2018Sponsors/${companyName}`)
 }
 
-const SponsorsPage = ({ data }) => {
-  // const grabTieredSponsors = (tier) => {
-  // 	const sponsorsArray = data.allSponsorsDataJson.edges.find(data => data.node.title === tier);
-  // 	if(sponsorsArray.node.items.length > 0) {
-  // 		return sponsorsArray.node.items.map((i,index) => (
-  // 			<div className={`col-md-6 col-lg-3 sponsorCard} ${tier === 'gold' ? 'col-lg-4' : 'col-lg-2'}`}>
-  // 				<div className="card border-0 transform-on-hover cardImgContainer">
-  // 					<a className="lightbox" href={i.url} target="_blank">
-  // 						<img style={`{width: tier === 'gold' ? '100%' : '75%'},${mobileLogo}]`} src={getImageUrl(i.image)} alt={`${i.name}`} className="card-img-top" />
-  // 					</a>
-  // 				</div>
-  // 			</div>
-  // 		));
-  // 	}
-  // }
+const SponsorRow = ({ node: { title, items: sponsors } }) => {
+  let className = title === 'gold' ? 'col-6 col-md-4' : 'col-4'
+  return (
+    <div className="container">
+      <div className="headingContainer">
+        <h2 className="sponsorsLevel">{title}</h2>
+      </div>
+      <div>
+        <div className="row rowStyling">
+          {sponsors.map(sponsor => (
+            <div className={`col-md-6 col-lg-3 sponsorCard ${title === 'gold' ? 'col-lg-4' : 'col-lg-2'}`}>
+              <div className="card border-0 transform-on-hover cardImgContainer">
+                <a className="lightbox" href={sponsor.url} target="_blank">
+                  <img src={getImageUrl(sponsor.image)} alt={`${sponsor.name}`} className="card-img-top mobileLogo" />
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
 
+const SponsorsPage = ({ data }) => {
+  let sponsors = data.allSponsorsDataJson.edges
   return (
     <Layout
       title="Sponsors"
@@ -31,24 +41,11 @@ const SponsorsPage = ({ data }) => {
       background={
         'https://images.unsplash.com/photo-1519999482648-25049ddd37b1?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=0471f46a1cda51342fdecf9a8c42349f&auto=format&fit=crop&w=1852&q=80'
       }>
-      {/* <div className="gallery-block">
-				<div className="container">
-					<div className="headingContainer">
-							<h2 className="sponsorsLevel">Gold</h2>
-					</div>
-					<div className="row rowStyling" >
-							{grabTieredSponsors('gold')}
-					</div>
-				</div> 
-				<div className="container">
-					<div className="headingContainer">
-							<h2 className="sponsorsLevel">Silver</h2>
-					</div>
-					<div className="row rowStyling">
-							{grabTieredSponsors('silver')}
-					</div>
-				</div> 
-			</div> */}
+      <div className="gallery-block">
+        {sponsors.map(level => (
+          <SponsorRow {...level} />
+        ))}
+      </div>
     </Layout>
   )
 }

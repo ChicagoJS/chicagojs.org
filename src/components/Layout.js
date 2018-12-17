9fad54903f9bdf9330aadf1b2787b6caa003877d
Sebastian <sebbykurp@gmail.com>
2018-12-17 16:17:20 -0600

    Added icons via FontAwesome.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -28,6 +28,12 @@ export default ({
         integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
         crossOrigin="anonymous"
       />
+      <link
+        rel="stylesheet"
+        href="https://use.fontawesome.com/releases/v5.6.1/css/all.css"
+        integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP"
+        crossorigin="anonymous"
+      />
       <title>{title}</title>
     </Helmet>
     {!hideNavbar && <Navbar />}

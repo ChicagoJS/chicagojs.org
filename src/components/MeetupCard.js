f720c399dafecc46bd22bb15bd475bf82a81b6d3
Alek Shnayder <alek.shnayder@gmail.com>
2018-12-19 12:49:54 -0600

    Added meetup API call to get total member count, styling

--- src/components/MeetupCard.js
+++ src/components/MeetupCard.js
@@ -1,21 +1,60 @@
 import React from 'react'
 import PropType from 'prop-types'
 import './MeetupCard.css'
+import fetchJsonp from 'fetch-jsonp'
 
-const MeetupCard = ({ name, description, urlname, category }) => (
-  <div className="col-sm-6 meetup">
-    <div className="card mb-4">
-      <div className="card-body">
-        <h5 className="card-title">{name}</h5>
-        <p className="card-text">{description}</p>
-        <a href={`https://www.meetup.com/${urlname}`} className="card-link">
-          Learn More
-        </a>
-        <div className={`category ${category}`}>{category}</div>
+class MeetupCard extends React.Component {
+  _isMounted = false
+
+  state = {
+    totalMembers: 0
+  }
+
+  componentDidMount() {
+    this._isMounted = true
+
+    fetchJsonp(`https://api.meetup.com/${this.props.urlname}`)
+      .then(response => response.json())
+      .then(json => {
+        if (this._isMounted && json.data) {
+          this.setState({
+            totalMembers: json.data.members
+          })
+        }
+      })
+      .catch(e => {
+        console.log('parsing failed', e)
+      })
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false
+  }
+
+  render() {
+    const { name, description, urlname, category } = this.props
+
+    return (
+      <div className="col-sm-6 meetup">
+        <div className="card mb-4">
+          <div className="card-body">
+            <h5 className="card-title">{name}</h5>
+            <p className="card-text">{description}</p>
+            <a href={`https://www.meetup.com/${urlname}`} className="card-link">
+              Learn More
+            </a>
+            <div className={`category ${category}`}>{category}</div>
+            {this.state.totalMembers !== 0 && (
+              <div className="members">
+                Total Members: <span className="memberCount">{this.state.totalMembers}</span>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-)
+    )
+  }
+}
 
 MeetupCard.propTypes = {
   name: PropType.string.isRequired,

4fd1af0814901ab6fcdb698aec61c63f8d6b67bd
Alek Shnayder <alek.shnayder@gmail.com>
2018-12-19 13:58:29 -0600

    Minor: Added Favicons, Meta Description, Iterator Keys (#33)
    - Added favicons to remove related console error from coming up on deployed site
    - Added meta description for SEO per Chrome Devtool's lighthouse audit
    - Added keys to iterators, removes console errors in dev

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import Helmet from 'react-helmet'
+import favicon32 from '../images/favicon/favicon-32x32.png'
+import favicon16 from '../images/favicon/favicon-16x16.png'
+import faviconICO from '../images/favicon/favicon.ico'
 import MailingListForm from './MailingListForm'
 import Navbar from './Navbar'
 import PageHeader from './PageHeader'
@@ -27,6 +30,10 @@ export default ({
         integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
         crossOrigin="anonymous"
       />
+      <link rel="icon" type="image/png" href={favicon32} sizes="32x32" />
+      <link rel="icon" type="image/png" href={favicon16} sizes="16x16" />
+      <link rel="shortcut icon" type="image/x-icon" href={faviconICO} />
+      <meta name="description" content="Uniting Chicago's JavaScript Community" />
       <title>{title}</title>
     </Helmet>
     {!hideNavbar && <Navbar />}

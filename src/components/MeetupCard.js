d8e766934cec7df1a7b0fbe7d481561e69f30ddb
Alek Shnayder <alek.shnayder@gmail.com>
2019-01-11 10:14:45 -0600

    Added bootstrap container inside meetup group with upcoming event from api

--- src/components/MeetupCard.js
+++ src/components/MeetupCard.js
@@ -1,24 +1,41 @@
 import React from 'react'
 import PropType from 'prop-types'
 import fetchJsonp from 'fetch-jsonp'
+import { format } from 'date-fns'
 import './MeetupCard.css'
 
 class MeetupCard extends React.Component {
   _isMounted = false
 
   state = {
-    totalMembers: 0
+    totalMembers: 0,
+    upcomingEvent: {}
   }
 
   componentDidMount() {
     this._isMounted = true
 
+    //fetches total members of group & saves to state
     fetchJsonp(`https://api.meetup.com/${this.props.urlname}`)
       .then(response => response.json())
       .then(json => {
         if (this._isMounted && json.data) {
           this.setState({
             totalMembers: json.data.members
+          })
+        }
+      })
+      .catch(e => {
+        console.log('parsing failed', e)
+      })
+
+    //fetches upcoming event of group & saves to state
+    fetchJsonp(`https://api.meetup.com/${this.props.urlname}/events?page=1`)
+      .then(response => response.json())
+      .then(json => {
+        if (this._isMounted && json.data.length) {
+          this.setState({
+            upcomingEvent: json.data[0]
           })
         }
       })
@@ -33,6 +50,7 @@ class MeetupCard extends React.Component {
 
   render() {
     const { name, description, urlname, category } = this.props
+    const { upcomingEvent } = this.state
 
     return (
       <div className="col-sm-6 meetup">
@@ -43,14 +61,33 @@ class MeetupCard extends React.Component {
               <span className={`badge badge-pill ${category}`}>{category}</span>
             </h5>
             <p className="card-text">{description}</p>
-            <a href={`https://www.meetup.com/${urlname}`} target="_blank" className="btn btn-outline-primary">
-              Learn More
+            <a href={`https://www.meetup.com/${urlname}`} target="_blank" className="btn btn-outline-primary join">
+              Join Group
             </a>
             {this.state.totalMembers !== 0 && (
               <span className="badge badge-light">
                 Total Members: <span className="memberCount">{this.state.totalMembers}</span>
               </span>
             )}
+            <div className="card bg-light mw-100 upcoming">
+              <div className="card-header">Upcoming Event</div>
+              <div className="card-body">
+                {upcomingEvent.name ? (
+                  <div>
+                    <h6 className="card-title">{upcomingEvent.name}</h6>
+                    <p className="card-text">
+                      <strong>Date: </strong>
+                      {format(new Date(upcomingEvent.time), 'MMM DD, YYYY h:mm a')}
+                    </p>
+                    <a href={upcomingEvent.link} target="_blank" className="btn btn-outline-secondary">
+                      RSVP Event
+                    </a>
+                  </div>
+                ) : (
+                  <p className="card-text">None Scheduled</p>
+                )}
+              </div>
+            </div>
           </div>
         </div>
       </div>

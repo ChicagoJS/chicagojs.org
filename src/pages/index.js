90e0229ee194ed243121beb6c828a40e2f4779a7
Dylan Irlbeck <irlbeck2@illinois.edu>
2019-07-29 09:14:27 -0500

    Added JSCamp announcement to home page (#38)
    * Added JSCamp announcement to home page
    
    * Removing unnecessary import
    
    * Changing color of 'here' link to match JSCamp icon

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import './IndexPage.css'
+import jscamplogo from '../images/jscamplogo.svg'
+import PropTypes from 'prop-types'
 
 const LANGUAGES = ['GatsbyJS', 'GraphQL', 'React', 'Emotion.sh', 'Node', 'ExpressJs']
 
-const IndexPage = () => (
-  <Layout hidePageHeader>
+const HomePage = () => {
+  return (
     <div className="jumbotron jumbotron-fluid index-page-body">
-      <div className="container">
+      {/* <div className="container">
         <h1 className="display-4">ChicagoJS</h1>
         <p className="lead">
           ChicagoJS is currently under development. If you'd like to help,{' '}
@@ -19,9 +21,48 @@ const IndexPage = () => (
             <li key={`lang-${l}`}>{l}</li>
           ))}
         </ul>
+      </div> */}
+      <JSCampBanner />
+    </div>
+  )
+}
+
+const BannerAdImage = ({ imageURL }) => <img src={imageURL} width="400" />
+
+const BannerAdHeadline = ({ text, linkToTickets }) => (
+  <p>
+    {text} <br />
+    Get your tickets <a href={linkToTickets}>here!</a>
+  </p>
+)
+
+const JSCampBanner = () => {
+  return (
+    <div className="jscampcontainer">
+      <BannerAdImage imageURL={jscamplogo} />
+      <div className="headline">
+        <BannerAdHeadline
+          text="JSCamp 2019 is happening September 14, 2019!"
+          linkToTickets="https://ti.to/chicagojs/chicago-js-camp-2019"
+        />
       </div>
     </div>
+  )
+}
+
+const IndexPage = () => (
+  <Layout hidePageHeader>
+    <HomePage />
   </Layout>
 )
+
+BannerAdImage.propTypes = {
+  imageURL: PropTypes.string
+}
+
+BannerAdHeadline.propTypes = {
+  text: PropTypes.string,
+  linkToTickets: PropTypes.string
+}
 
 export default IndexPage
